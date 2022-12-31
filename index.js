21333ed1445e3ceacefc0aeff229822b34f31d2f
Mohammedansari808 <mohammedansari808@gmail.com>
2022-12-31 22:17:11 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import rooms from "./room.json"assert { type: "json" }
 import * as dotenv from 'dotenv'
-dotenv.config()
 const app = express();
 const PORT = process.env.PORT;
 const room =
